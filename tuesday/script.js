0cc8532a32180b80069f1603b2ce3b1c9a2461eb
Bennah <bennah955@gmail.com>
2026-01-27 15:15:47 +0300

    wrapped up tuesday class

--- tuesday/script.js
+++ tuesday/script.js
@@ -16,12 +16,58 @@
 // };
 
 // odd or even or otherwise checker
+// let num = 4;
+//  if(isNaN(num)) {
+//   console.log("This is not a valid number");
+//  } else if (num % 2 === 0 ){
+//   console.log(`${num} is an even number`);
+//  } else {
+//   console.log(`${num} is an odd number`);
+//  };
 
-let num = 4;
- if(isNaN(num)) {
-  console.log("This is not a valid number");
- } else if (num % 2 === 0 ){
-  console.log(`${num} is an even number`);
- } else {
-  console.log(`${num} is an odd number`);
- };
+// if (number % 2 === 0 ){
+//   console.log(`${number} is an even number`);
+// } 
+// else if ()
+
+
+
+
+// A program that assigns grade based on a students score
+// let mark = Number(prompt("Enter mark: "));
+
+// if (mark >100 || mark<0){
+//   console.log("Invalid score")
+// }else if(mark >= 80 ) {
+//   console.log("A");
+// } else if (mark >= 70) {
+//   console.log("B");
+// } else if (mark >= 60){
+//   console.log("C");
+// } else if (mark >= 50){
+//   console.log("D");
+// } else if (mark < 50){
+//   console.log ("E")
+// } else {
+//   console.log("Invalid mark! See Instructor ASAP!")
+// };
+
+
+
+
+
+let mark = Number(prompt("Enter mark: "));
+
+if (score >= 0 && score < 50){
+  console.log("E");
+} else if (score >= 50 && score < 60){
+  console.log("D");
+} else if (score >= 60 && score < 70){
+  console.log("C");
+} else if (score >= 70 && score < 80){
+  console.log("B");
+} else if (score >= 80 && score < 100){
+  console.log("A");
+} else {
+  console.log("Invalid score!");
+}
