22f45fb6d2329f061ad333881704cdcd75a12d41
Bennah <bennah955@gmail.com>
2026-01-27 11:52:22 +0300

    This is a week 3 learning alongside instructor- each folder is the day

---
+++ monday/script.js
@@ -0,0 +1,30 @@
+// let name = "Benard";
+// let interest = "cars and the engineering behind them"
+// const favorite = "Nissain Skyline R35"
+// let predecessor = "Nissan Skyline R34"
+
+
+// // template strings
+// console.log( `My name is ${name}, and I am interested in ${interest}. My all time favorite car is the ${favorite}, which evolved from the ${predecessor}` );
+
+// comparison operators
+// let num1 = 54;
+// let num2 = 45;
+
+// console.log(num1===num2);
+// console.log(num1>num2);
+// console.log(num1<num2);
+// console.log(num1>=num2);
+// console.log(num1<=num2);
+
+// logical operators
+
+let name = "Benard";
+let isStudent = false;
+
+if (isStudent) {
+  console.log(`Welcome to mcTaba labs!`);
+}
+else {
+  console.log("Become a student?")
+};
---
+++ tuesday/script.js
@@ -0,0 +1,27 @@
+// Type Conversion/casting
+// we convert user input from string to number
+// All numbers can be converted to strings but not all strings can be converted too numbers
+
+// conditional statements
+// if...else if
+
+// let mark = 15;
+
+// if (mark >= 12) {
+//   console.log("Above average");
+// } else if (9 < mark < 12 ) {
+//   console.log("You can do better");
+// } else {
+//   console.log("Yo have not met the requirements");
+// };
+
+// odd or even or otherwise checker
+
+let num = 4;
+ if(isNaN(num)) {
+  console.log("This is not a valid number");
+ } else if (num % 2 === 0 ){
+  console.log(`${num} is an even number`);
+ } else {
+  console.log(`${num} is an odd number`);
+ };
