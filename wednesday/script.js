92d550a90ada6e92cd71131484b88287310d1bf2
Bennah <bennah955@gmail.com>
2026-01-28 13:57:03 +0300

    Wednesday class

---
+++ wednesday/script.js
@@ -0,0 +1,76 @@
+// for (let num=1; num <= 10; num++){
+//   console.log(num);
+// }
+
+// a for loop thatt prints numbers from 0 to 20
+// for (let number = 0; number <= 20; number++){
+//   console.log(`Number ${number}`);
+// }
+
+// for (let number = 0; number <=100; number++){
+//   if(number % 2 === 0){
+//     console.log(`${number} is an even number`);
+//   }
+// }
+
+// arrays & looping through them
+// names = ["Ben", "Ken", "Jane", "Jade"];
+//logs names ata certain index- the specified index
+// console.log(names[3]);
+
+// console.log(names);
+// names.push("Bona"); //Adds elements infront of the array
+// console.log(`${names} - push`);
+
+// names.pop(); //removes element infront of the array
+// console.log(`${names} - pop`);
+
+// names.unshift("Ogeto"); //add element at the beginning
+// console.log(`${names} - unshift`);
+
+// names.shift(); //removes element from the beginning
+// console.log(`${names} - shift`);
+
+// looping
+// for (let num = 0; num <=names.length; num++){
+//   console.log(names[num]);
+// }
+
+
+
+// A JS program that returns the maximum age in the array below
+// ages = [34, 78, 12, 56, 89, 98];
+
+// console.log(Math.max(...ages));
+
+
+// Objects
+// It is used to store various keyed collections and more complex entities
+
+
+
+
+
+
+
+
+
+
+// list of objects
+randomList = [
+  {
+    name: {
+      occupation: "Teacher",
+      nationality: "Kenyan"
+    },
+    hobbies: {
+      recreational: "swimming",
+      sports: "Football"
+    }
+  },
+  {
+    brand: "Tesla",
+    model: "Model S",
+    Year: "2024"
+  }
+]
